b49e6f71a4d3f263d536c2d9a7e427ae1de116c1
Prakash <prakashkbehera@gmail.com>
2024-04-06 18:28:08 +0530

    intitial commit with new project

---
+++ src/main/java/com/springboot/Sonarqube/SpringbootSonarqubeGitDemoProjectApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.Sonarqube;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootSonarqubeGitDemoProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootSonarqubeGitDemoProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springboot/Sonarqube/SpringbootSonarqubeGitDemoProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.Sonarqube;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootSonarqubeGitDemoProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
